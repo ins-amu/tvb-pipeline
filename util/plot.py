da72f6fca5f0ba12c4455e3979629247d12ed28b
Viktor Sip <viktor.sip@univ-amu.fr>
2017-12-20 09:56:23 +0100

    Plot T1 + electrodes for visual check

--- util/plot.py
+++ util/plot.py
@@ -4,20 +4,22 @@ import os
 import logging
 import numpy as np
 import re
+import textwrap
 import zipfile
+
+
+import nibabel as nib
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-
 class NamedPoints():
     def __init__(self, fl):
         data = np.genfromtxt(fl, dtype=None)
         self.xyz = np.array([[l[1], l[2], l[3]] for l in data])
         self.names = [l[0].decode('ascii') for l in data]
-
 
 class Contacts(NamedPoints):
     contact_name_regex = re.compile("^([A-Za-z]+[']?)([0-9]+)$")
@@ -34,6 +36,13 @@ class Contacts(NamedPoints):
             if elec_name not in self.electrodes:
                 self.electrodes[elec_name] = []
             self.electrodes[elec_name].append(i)
+
+    def get_elec(self, name):
+        match = self.contact_name_regex.match(name)
+        if match is None:
+            return None
+
+        return match.groups()[0]
 
 
 def seeg_elecs(tvbzip_file, seegxyz, out_fig):
@@ -70,6 +79,126 @@ def seeg_elecs(tvbzip_file, seegxyz, out_fig):
     plt.legend(loc='upper left')
     plt.tight_layout()
     plt.savefig(out_fig)
+
+ras_slice_indices = {
+    'transversal': ([1, 0], 2),
+    'coronal':     ([2, 0], 1),
+    'sagittal':    ([2, 1], 0)
+}
+
+
+
+def get_slice(img, slice_type, ras_coord):
+    eps = 1e-20
+
+    affine = img.affine
+
+    # Check the the transformation
+    assert np.all(np.sum(np.abs(affine[:3, :3]) > eps, axis=1) == 1)
+
+    slice_dims, slice_param = ras_slice_indices[slice_type]
+
+    ras_coords = [ras_coord for _ in range(3)] + [1]
+    ras_coords[slice_dims[0]] = ras_coords[slice_dims[1]] = 0
+    vox_coords = [int(x) for x in np.linalg.solve(affine, ras_coords)[0:3]]
+
+    ax0 = np.where(np.abs(affine[slice_dims[0], 0:3]) > eps)[0][0]
+    ax1 = np.where(np.abs(affine[slice_dims[1], 0:3]) > eps)[0][0]
+
+    slicer = vox_coords
+    slicer[ax0] = slice(None, None, 1 if affine[slice_dims[0], ax0] > 0 else -1)
+    slicer[ax1] = slice(None, None, 1 if affine[slice_dims[1], ax1] > 0 else -1)
+    slicer = tuple(slicer)
+
+    data = img.get_data()[slicer]
+
+    # Extent
+    corner_0 = np.dot(affine, [0, 0, 0, 1])[0:3]
+    corner_1 = np.dot(affine, np.append(img.shape, 1))[0:3]
+    extent_ax0 = sorted([corner_0[slice_dims[0]], corner_1[slice_dims[0]]])
+    extent_ax1 = sorted([corner_0[slice_dims[1]], corner_1[slice_dims[1]]])
+
+    # Transpose if necessary
+    if ax0 > ax1:
+        data = data.T
+
+    extent = extent_ax1 + extent_ax0
+
+    return data, extent
+
+
+
+def plot_t1_elec(t1_img_file, elec_img_file, elec_pos_file, contact_name, filename):
+    t1_img = nib.load(t1_img_file)
+    elec_img = nib.load(elec_img_file)
+    contacts = Contacts(elec_pos_file)
+
+    labels = ["L --- R", "P --- A", "I --- S"]
+
+    t1_lims = np.min(t1_img.get_data()), np.max(t1_img.get_data())
+    elec_lims = np.min(elec_img.get_data()), np.max(elec_img.get_data())
+
+    contact_ind = contacts.names.index(contact_name)
+    elec_inds = contacts.electrodes[contacts.get_elec(contact_name)]
+
+    fig = plt.figure(figsize=(18, 18))
+    for i, slice_type in enumerate(['coronal', 'sagittal', 'transversal']):
+        plt.subplot(2, 2, i + 1)
+
+        slice_dims, slice_param = ras_slice_indices[slice_type]
+
+        contact_xy = contacts.xyz[contact_ind][slice_dims]
+        contact_z = contacts.xyz[contact_ind][slice_param]
+
+        t1_img_data, t1_img_extent = get_slice(t1_img, slice_type, contact_z)
+        plt.imshow(t1_img_data, extent=t1_img_extent,
+                   vmin=t1_lims[0], vmax=t1_lims[1],
+                   cmap='Greys_r', origin='lower', zorder=0)
+
+        elec_img_data, elec_img_extent = get_slice(elec_img, slice_type, contact_z)
+        plt.imshow(elec_img_data, extent=elec_img_extent,
+                   vmin=elec_lims[0], vmax=elec_lims[1],
+                   cmap='hot', origin='lower', zorder=1, alpha=0.6)
+
+        plt.scatter([contact_xy[1]], [contact_xy[0]], facecolors='none', edgecolors='g',
+                    s=60, zorder=2)
+
+        for ind in elec_inds:
+            if ind == contact_ind:
+                continue
+            plt.scatter([contacts.xyz[ind, slice_dims[1]]], [contacts.xyz[ind, slice_dims[0]]],
+                        c='g', s=10, marker='x', zorder=2)
+
+        plt.xlabel(labels[slice_dims[1]], fontsize=16)
+        plt.ylabel(labels[slice_dims[0]], fontsize=16)
+
+    # Text description
+    ax = plt.subplot(2, 2, 4)
+    ax.axis('off')
+    ax.text(0.1, 0.9, contact_name, size=16, ha="left")
+
+    desc = """
+    In black-white: T1 scan
+    In black-red-yellow: Scan with implanted electrodes
+    In green: electrode position
+    """
+    ax.text(0.1, 0.7, textwrap.dedent(desc), size=12, ha="left")
+
+    plt.tight_layout(pad=1.4)
+    plt.savefig(filename)
+    plt.close()
+
+
+def plot_t1_plus_elecs(t1_img_file, elec_img_file, elec_pos_file, out_direc):
+    if not os.path.isdir(out_direc):
+        os.makedirs(out_direc)
+
+    contacts = Contacts(elec_pos_file)
+    for elec, elec_inds in contacts.electrodes.items():
+        contact_name = contacts.names[elec_inds[0]]
+        contact_name_safe = contact_name.replace("'", "p")
+        filename = os.path.join(out_direc, "t1_elec-img_elec-pos_%s.png" % contact_name_safe)
+        plot_t1_elec(t1_img_file, elec_img_file, elec_pos_file, contact_name, filename)
 
 
 
