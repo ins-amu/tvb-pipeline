d556e414973d4d3ed6e6630e2de814f43560bb41
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2018-07-19 09:24:48 -0400

    sfreq & plot dataset

--- util/vep_preprocess.py
+++ util/vep_preprocess.py
@@ -13,20 +13,25 @@ import zipfile
 
 import numpy as np
 import scipy.signal
-import mne; mne.verbose('WARNING')
-import pylab as pl
+import mne
+
+try:
+    import matplotlib as mpl
+    mpl.use('agg')
+except:
+    pass
 import pycmdstan as pcs
 
 
 def compute_raw_slp(raw, cfg):
-    nperseg: int = cfg.get('nperseg', 4096)
+    nperseg: int = cfg.get('nperseg', 4 * int(raw.info['sfreq']))
     hpf: float = cfg.get('hpf', 10.0)
     lpf: float = cfg.get('lpf', 100.0)
     Cs = []
     for y in raw._data:
         F, T, C = scipy.signal.spectrogram(
             y, raw.info['sfreq'], nperseg=nperseg)
-        fmask = np.ones(C.shape[0], 'i')
+        fmask = np.ones(F.shape, 'bool')
         if hpf:
             fmask *= F > hpf
         if lpf:
@@ -93,7 +98,7 @@ def read_all_fif(subj_proc_dir, cfg: dict):
     slps_ = []
     is_first_ = []  # 1 if first samp of seizure
     for slp, ch in zip(slps, chs):
-        im = np.array([ch.index(p) for p in picks])
+        im = np.array([i for i, c in enumerate(ch) if c in picks])
         is_first = np.zeros(slp.shape[1], 'i')
         is_first[0] = 1
         slps_.append(slp[im])
@@ -174,13 +179,51 @@ def build_and_save_one(subj_proc_dir, cfg=None):
         print(f'skipping existing {Rfname}')
         return
     data = build_data(subj_proc_dir, cfg)
+    plot_dataset(subj_proc_dir, data)
     pcs.rdump(Rfname, data)
 
 
 def build_and_save_all_retro():
     for id in retro_ids():
+        print(id)
         build_and_save_one(retro_proc_dir(id))
 
+
+def plot_dataset(subj_proc_dir, data):
+    import matplotlib as mpl
+    mpl.use('agg')
+    import pylab as pl
+    # TODO more informative ticks/labels
+    pl.figure(figsize=(10, 6))
+    pl.subplot(121)
+    pl.imshow(data['seeg'], vmin=8, vmax=14, aspect='auto', cmap='binary')
+    for ft in np.argwhere(data['is_first'])[:, 0]:
+        pl.axvline(ft, color='r')
+    pl.title('Concatenated Seizures')
+    pl.xlabel('Time Window (2 s)')
+    pl.ylabel('sEEG Monopolar Contact')
+    pl.grid(1)
+    pl.subplot(222)
+    pl.imshow(data['gain'], aspect='auto', cmap='binary')
+    pl.title('Gain Matrix (Destrieux -> sEEG)')
+    pl.xlabel('Destrieux ROI')
+    pl.ylabel('sEEG Monopolar Contact')
+    pl.grid(1)
+    pl.subplot(224)
+    pl.hist(np.log(data['counts_triu'] + 1), 30, color='k')
+    pl.title('log Track Count Histogram')
+    pl.xlabel('log Track Count')
+    pl.ylabel('# log Track Count')
+    pl.grid(1)
+    pl.tight_layout()
+    pl.savefig(os.path.join(
+        subj_proc_dir, 'vep', 'data.png'))
+
+# testing
+# spd = retro_proc_dir('id023_br')
+# data = build_data(spd, {'hpf': 20.0, 'lpf': None})
+# plot_dataset(spd, data)
+# print('done')
 
 if __name__ == '__main__':
     subj_proc_dir, = sys.argv[1:]
